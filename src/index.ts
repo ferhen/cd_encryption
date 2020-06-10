98f3873862b156da3df0260c068e609a1191436d
Fernando Eckstein <fernando_eckstein@hotmail.com>
2020-06-09 22:56:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,79 @@
+// core
+import { exists, readJson, writeJson } from 'https://deno.land/std/fs/mod.ts';
+// imports
+import { sha1 } from 'https://denopkg.com/chiefbiiko/sha1/mod.ts';
+// models
+import { IAnswer } from './model.ts';
+
+const TOKEN = '';
+const PATH = './data/answer.json';
+const GET_API = `https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${TOKEN}}`;
+const POST_API = `https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${TOKEN}`;
+
+async function run(): Promise<void> {
+    const data = await loadData(PATH, GET_API);
+    data.decifrado = decrypt(data);
+    data.resumo_criptografico = sha1(data.decifrado, "utf8", "hex") as string;
+    const result = await sendData(POST_API, data);
+    console.log(await result.json());
+}
+
+async function loadData(path: string, api: string): Promise<IAnswer> {
+    const fileExists = await exists(path);
+    const data = fileExists ? await loadFromPath(path) : await loadFromAPI(api);
+    if (!fileExists) { await writeJson(path, data); }
+    return data as IAnswer;
+}
+
+async function loadFromPath(path: string): Promise<IAnswer> {
+    const data = await readJson(path);
+    return data as IAnswer;
+}
+
+async function loadFromAPI(api: string): Promise<IAnswer> {
+    const res = await fetch(api);
+    const data = await res.json();
+    return data as IAnswer;
+}
+
+function decrypt({ cifrado, numero_casas }: IAnswer): string {
+    const aCharCode = "a".charCodeAt(0);
+    const zCharCode = "z".charCodeAt(0);
+
+    const decrypted = cifrado
+        .split(' ')
+        .map(word => word.split('')
+            .map(char => {
+                const encryptedCharCode = char.charCodeAt(0);
+                let decryptedCharCode: number;
+
+                if (encryptedCharCode >= aCharCode && encryptedCharCode <= zCharCode) {
+                    decryptedCharCode = encryptedCharCode - numero_casas;
+                    if (decryptedCharCode > zCharCode) {
+                        decryptedCharCode -= 26;
+                    } else if (decryptedCharCode < aCharCode) {
+                        decryptedCharCode += 26;
+                    }
+                } else {
+                    decryptedCharCode = encryptedCharCode;
+                }
+
+                return String.fromCharCode(decryptedCharCode);
+            }).join('')
+        ).join(' ');
+
+    return decrypted;
+}
+
+async function sendData(api: string, data: IAnswer): Promise<Response> {
+    const form = new FormData();
+    form.append("answer", new Blob([JSON.stringify(data)]));
+
+    const response = await fetch(api, {
+        method: "POST",
+        body: form
+    });
+    return response;
+}
+
+run();
---
+++ src/model.ts
@@ -0,0 +1,7 @@
+export interface IAnswer {
+    numero_casas: number;
+    token: string;
+    cifrado: string;
+    decifrado: string;
+    resumo_criptografico: string;
+}
